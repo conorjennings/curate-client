49bcadef49188d3856841ea10dad30abaaa84cba
danielleletarte <danielle.a.letarte@gmail.com>
2017-06-05 14:02:04 -0400

    Build out logic to render the results of the INDEX request to the Dashboard component on Sign In.
    Next steps. Building Create form on front end.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Card, CardTitle } from 'material-ui/Card';
+import mui from 'material-ui';
+import FavoriteIcon from 'react-material-icons/icons/action/favorite';
+import SlowIcon from 'react-material-icons/icons/action/schedule';
+import SustainableIcon from 'react-material-icons/icons/action/cached';
+import IndependentIcon from 'react-material-icons/icons/action/face';
+
 // Theme import required to get Material-UI working
 
 const Dashboard = ({retailProfiles}) => {
@@ -8,7 +14,16 @@ const Dashboard = ({retailProfiles}) => {
   console.log('retailProfiles from props ', retailProfileArray)
 
   const listItems = retailProfileArray.map((retailProfile) =>
-    <li key={retailProfile.id}>{retailProfile.name}</li>
+    <div key={retailProfile.id}>
+    <a href={retailProfile.siteUrl}>{retailProfile.name}</a>
+    <p>{retailProfile.notes}</p>
+    <div>
+    {retailProfile.vegan && <FavoriteIcon/>}
+    {retailProfile.slow && <SlowIcon/>}
+    {retailProfile.sustainable && <SustainableIcon/>}
+    {retailProfile.independent && <IndependentIcon/>}
+    </div>
+    </div>
   )
 
   console.log('listItems is ', listItems)
@@ -16,9 +31,9 @@ const Dashboard = ({retailProfiles}) => {
   return (
   <Card className="container">
   <CardTitle title="Dashboard" subtitle="Cool stuff goes here." />
-  <ul>
+  <div>
     {listItems}
-  </ul>
+  </div>
   </Card>
   );
 }
